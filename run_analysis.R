cb00f4c514652ee6cd66e619696b49c5a57675c9
uiroumaru <100431194+uiroumaru@users.noreply.github.com>
2022-08-08 01:49:44 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -31,10 +31,10 @@ head(x_train);dim(x_train)
 head(y_train);dim(y_train)
 
 #1.Merges the training and the test sets to create one data set.
-X <- rbind(x_train, x_test)
-Y <- rbind(y_train, y_test)
+X_train_test <- rbind(x_train, x_test)
+Y_train_test <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
-Merged_Data <- cbind(Subject, Y, X)
+Merged_Data <- cbind(Subject, Y_train_test, X_train_test)
 
 #2Extracts only the measurements on the mean and standard deviation for each measurement. 
 library(dplyr)
